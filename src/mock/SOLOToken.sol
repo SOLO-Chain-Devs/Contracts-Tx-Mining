db96e0c53ff1264b9795a327ac78ed55c96c0b20
0xheartcode <0xheartcode@gmail.com>
2025-01-17 00:01:56 +0100

    Patch: typo

--- src/mock/SOLOToken.sol
+++ src/mock/SOLOToken.sol
@@ -2,9 +2,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
-
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
